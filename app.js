f0dfa4628b618569242f8df826ed58fee1ef9b2c
kphacko <karan2000patil@gmail.com>
2020-07-06 13:36:47 +0530

    login routes changes

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const app = express();
 
 
 app.use(express.json());
-app.use(require('body-parser').urlencoded({extended:true,limit:'100mb'}));
-app.use(require('body-parser').json({limit:'100mb'}));
-app.set('view engine','ejs');
+app.use(require('body-parser').urlencoded({ extended: true, limit: '100mb' }));
+app.use(require('body-parser').json({ limit: '100mb' }));
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 
-app.get('/login',(req,res)=>{
-  res.render('login');
+app.get('/login', (req, res) => {
+    res.render('/sdflogin');
+
 });
-app.get ('/register',(req,res)=>{
+app.get('/register', (req, res) => {
     res.render('register');
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log('heared on ' + process.env.PORT);
 });
